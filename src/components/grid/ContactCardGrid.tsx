b835a9ad5fb75366a341fd7920989db66140225a
knowledgeAlan <knowledgeAlan>
2025-01-24 14:32:57 +0800

    Card Grid

--- src/components/grid/ContactCardGrid.tsx
+++ src/components/grid/ContactCardGrid.tsx
@@ -1,9 +1,48 @@
-
-
+import { Grid,container,Card, CardHeader, CardContent, Avatar, Typography,List, ListSubheader } from "@mui/material";
+import { contactData } from "../../Data/ContactData";
 export default function ContactCardGrid(){
     return (
-        <div>
-            Contact card grid
-        </div>
+        <Grid container spacing={2} sx={{width:700}}>
+            { contactData.map((contract)=>{
+                return (
+                  <Grid item key={contract.name}> 
+                    <Card sx={{width:300}}>
+                        <CardHeader
+                          title={contract.name}
+                          subheader={contract.role}
+
+                          avatar={
+                          <Avatar>
+                              {contract.name?.substring(0,1).toUpperCase()}
+                          </Avatar>}
+                        />
+
+                        <CardContent>
+                            <Typography>
+                              <b>Start Date:{contract.startDate}</b>
+                            </Typography>
+                            <Typography>
+                              <b>Work Preference:{contract.preference}</b>
+                            </Typography>
+                            <List
+
+                              sx={{listStyle: 'list-item',listStyleType:"circle",paddingLeft:2}}
+                              subheader={
+                                <ListSubheader sx={{right:16,position:"inherit",fontSize:"1.25rem",color:"black",paddingLeft:0}}>
+                                  Skills:
+                                </ListSubheader>
+                              }
+                            >
+                                { contract.skills?.map((skill)=>{
+                                  return <li style={{paddingBottom:"2px"}}>{skill}</li>
+                                })}
+                            </List>
+                        </CardContent>
+                    </Card>
+                  </Grid>  
+                )
+            })
+        }
+        </Grid>
     )
 }
--- src/data/ContactData.tsx
+++ src/data/ContactData.tsx
@@ -15,5 +15,11 @@ export const contactData:Array<FormValues> =[
 
     {id:1,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"},
     {id:2,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"},
-    {id:3,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"}
+    {id:3,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"},
+    {id:4,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"},
+    {id:5,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"},
+    {id:6,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"},
+    {id:7,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"},
+    {id:8,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"},
+    {id:9,name: "John",role: "Dev",skills:["React","Angular"],startDate:"2025-01-21",preference:"working from home"},
 ]
